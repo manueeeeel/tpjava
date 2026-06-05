cc112069a3f8ed78fc39824b328c45dfb3dbd55e
BenjaLV <benjalavalla8@gmail.com>
2026-06-04 21:13:25 -0300

    01-levante el fronted, agregue la carpeta view, corregi la direccion de los datos de la controladora y vincule el front en el main

---
+++ src/view/MainFrame.java
@@ -0,0 +1,10 @@
+package view;
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+    public MainFrame() {
+        setTitle("Main Frame");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
