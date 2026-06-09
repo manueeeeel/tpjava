12841b41a4afed18f13fe4965c19d20d35d8117b
LucioSedrani <luciosedrani07@gmail.com>
2026-06-09 13:02:45 -0300

    Saque el constructor de clase y le puse setters

--- src/Clases_utilizadas/clase.java
+++ src/Clases_utilizadas/clase.java
@@ -4,9 +4,13 @@ import jakarta.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name = "clase")
 public class clase{
     private String codigo,fecha,horario;
-    public clase(String cod,String fec,String ho){
+    public void setCodigo(String cod){
         codigo = cod;
+    }
+    public void setFecha(String fec){
         fecha = fec;
+    }
+    public void setHorario(String ho){
         horario = ho;
     }
     public String getCodigo(){
