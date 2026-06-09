8a42eab436a7b076a1250cccb0eac02973e58a84
LucioSedrani <luciosedrani07@gmail.com>
2026-06-09 12:59:11 -0300

    Saque el constructor vacio de clase

--- src/Clases_utilizadas/clase.java
+++ src/Clases_utilizadas/clase.java
@@ -4,7 +4,6 @@ import jakarta.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name = "clase")
 public class clase{
     private String codigo,fecha,horario;
-    public clase() {}
     public clase(String cod,String fec,String ho){
         codigo = cod;
         fecha = fec;
